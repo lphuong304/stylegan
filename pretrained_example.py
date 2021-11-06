5c40e45b29e3ca1457a731f6c0a8e91a8c3d5ffe
Nguyễn Ngọc Lan Phương <55471582+lphuong304@users.noreply.github.com>
2021-11-06 10:00:40 +0700

    Update pretrained_example.py

--- pretrained_example.py
+++ pretrained_example.py
@@ -20,7 +20,8 @@ def main():
     tflib.init_tf()
 
     # Load pre-trained network.
-    url = 'https://drive.google.com/uc?id=1MGqJl28pN4t7SAtSrPdSRJSQJqahkzUf' # karras2019stylegan-ffhq-1024x1024.pkl
+    url = 'https://drive.google.com/uc?id=1_vOyeUXLvOE2vHycmPNkq8Ku5yspCLF_' # karras2019stylegan-ffhq-1024x1024.pkl
+    
     with dnnlib.util.open_url(url, cache_dir=config.cache_dir) as f:
         _G, _D, Gs = pickle.load(f)
         # _G = Instantaneous snapshot of the generator. Mainly useful for resuming a previous training run.
